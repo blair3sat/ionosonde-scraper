ab532424bc2abec7c8ab874e800c6ba2eb127077
Andrew <doolie.healey@gmail.com>
2018-10-16 21:52:38 -0400

    Fiddled to no avail: POST body parameters given to the server return the equivalent of the GET page.

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ sma=now.subtract(6,"months");
 allSelects=[['at138', 'Athens'], ['rl052', 'Chilton'], ['db049', 'Dourbes'], ['ea036', 'El Arenosillo'], ['gm037', 'Gibilmanna'], ['jr055', 'Juliusruh'], ['le061', 'Lerwick'], ['lg178', 'Longyearbyen'],
 ['ly164', 'Lycksele'], ['mo155', 'Moscow'], ['pq052', 'Pruhonice'], ['ro041', 'Rome'], ['sd266', 'Salekhard'], ['psj5j', 'Stanley'], ['thj77', 'Thule (Qanaq)'],
 ['eb040', 'Tortosa'], ['tr169', 'Tromso'], ['mz152', 'Warsaw']];
-info={method:"post",url:"https://www.ukssdc.ac.uk/cgi-bin/digisondes/cost_database.pl",headers:{ 'Authorization': 'Basic ' + btoa('ryan.tse@blair3sat.com:') },body:{ 'retrieve': 'Retrieve data', 'ursi': 'at138', 'datetype': 'range', 'syear': sma.get("year").toString(), 'smonth': sma.get("month").toString(),
+info={method:"post",url:"https://www.ukssdc.ac.uk/cgi-bin/digisondes/cost_database.pl",headers:{ 'Authorization': 'Basic ' + btoa('ryan.tse@blair3sat.com:'), 'content-type' : 'application/x-www-form-urlencoded'},body:{ 'retrieve': 'Retrieve data', 'ursi': 'at138', 'datetype': 'range', 'syear': sma.get("year").toString(), 'smonth': sma.get("month").toString(),
 'sday': sma.get("month").toString(), 'shour': sma.get("hour").toString(), 'sminute': sma.get("minute").toString(), 'ssecond': sma.get("second").toString(), 'eyear': now.get("year").toString(), 'emonth': now.get("month").toString(), 'eday': now.get("date").toString(),
  'ehour': now.get("hour").toString(), 'eminute': now.get("minute").toString(), 'esecond': now.get("second").toString(), 'repeat': 'no', 'offset': '0', 'type': 'NHPC_Profiles', 'sort': 'Time', 'destination': 'File' },json:true}
 all_urls=[]
-console.log(info.body);
+console.log(info);
 for (i in allSelects) {
     info.body.ursi=allSelects[i][0];
     console.log(info.body.ursi);
-    request(info,(err,response,html)=>{
+    request.post(info,(err,response,html)=>{
       if(!err){
-      // let $=cheerio.load(html);
-      // console.log(html);
-      // let thePre=$("pre").eq(0);
-      // console.log(thePre.html());
-      // let ftpLink=$("pre").children("a").attr("href");
-      // all_urls.push([allSelects[i][1],ftpLink]);
-      // if(all_urls.length==allSelects.length){
-      //   console.log(all_urls);
-      // }
+      let $=cheerio.load(html);
+      console.log(html);
+      let thePre=$("pre").eq(0);
+      console.log(thePre.html());
+      let ftpLink=$("pre").children("a").attr("href");
+      all_urls.push([allSelects[i][1],ftpLink]);
+      if(all_urls.length==allSelects.length){
+        console.log(all_urls);
+      }
     }
     else{console.error(err)}
     });
